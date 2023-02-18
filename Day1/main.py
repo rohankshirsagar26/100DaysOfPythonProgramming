7ef454238fad4deba178dfccbcb6aa4bdc0614f2
rohankshirsagar <61232180+rohankshirsagar26@users.noreply.github.com>
2023-02-18 11:40:19 +0000

    input function

--- Day1/main.py
+++ Day1/main.py
@@ -8,3 +8,7 @@ print('Hello World\nHello World')
 
 # Concat two strings using +
 print('Hello' + ' ' + 'Rohan')
+
+# Input Function
+input('What is your name? ')
+print('Hello ' + input('What is your name?\n'))
