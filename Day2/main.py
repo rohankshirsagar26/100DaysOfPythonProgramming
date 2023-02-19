f569c8a65cfbdbc1df3f9b5868b9c103d2701a77
rohankshirsagar <61232180+rohankshirsagar26@users.noreply.github.com>
2023-02-19 13:05:20 +0000

    data type checking using type function

--- Day2/main.py
+++ Day2/main.py
@@ -17,3 +17,9 @@ print(123 + 345)
 
 True
 False
+
+
+num_char = len(input('What is your name? '))
+
+# type checking
+print(type(num_char))
--- main.py
+++ main.py
@@ -1,19 +1,6 @@
 #Data Types
 
-#String
+num_char = len(input('What is your name? '))
 
-print('Hello'[4])
-print('123'+'345')
-
-#Integer
-
-print(123 + 345)
-
-#Float
-
-3.1428
-
-#Boolean
-
-True
-False
+# type checking
+print(type(num_char))
