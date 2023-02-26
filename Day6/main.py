ee0e29d30551e82b228a8ffbdc96b837698b4f8e
rohankshirsagar <61232180+rohankshirsagar26@users.noreply.github.com>
2023-02-26 07:58:07 +0000

    Reeborg Challenge

--- Day6/main.py
+++ Day6/main.py
@@ -5,4 +5,7 @@ def my_function():
   print('Bye')
 
 my_function()
-  
+
+# Reeborg Challenege
+
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-# Functions
+# Reeborg Challenge
 
-def my_function():
-  print('Hello')
-  print('Bye')
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
 
-my_function()
+# Python Code
+
+def step_one():
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+
+def step_two():
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+
+for n in range(1, 13):
+    if n%2==1:
+        step_one()
+    else:
+        step_two()
