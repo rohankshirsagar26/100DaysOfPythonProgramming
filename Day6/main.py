8d3d54487bbba931c866bc59c343b2fbd8eeaec9
rohankshirsagar <61232180+rohankshirsagar26@users.noreply.github.com>
2023-02-26 09:21:53 +0000

    Reeborg Challenege with while and if

--- Day6/main.py
+++ Day6/main.py
@@ -66,3 +66,27 @@ number_of_hurdles = 6
 while not at_goal():
     one_hurdle()
     number_of_hurdles -= 1
+
+# Reeborg Challenege with while and if
+
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def skip_wall():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()   
+   
+while not at_goal():
+    if wall_in_front():
+        skip_wall()
+    elif front_is_clear():
+        move()
--- main.py
+++ main.py
@@ -1,32 +1,25 @@
 # While loop
 
-# Reeborg Challenege
+# Reeborg Challenege with while and if
 
-https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
 
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
 
-def step_one():
-    move()
+def skip_wall():
     turn_left()
     move()
     turn_right()
-
-def step_two():
     move()
     turn_right()
     move()
-    turn_left()
-    
-def one_hurdle():
-    step_one()
-    step_two()
-    
-number_of_hurdles = 6
+    turn_left()   
    
 while not at_goal():
-    one_hurdle()
-    number_of_hurdles -= 1
+    if wall_in_front():
+        skip_wall()
+    elif front_is_clear():
+        move()
