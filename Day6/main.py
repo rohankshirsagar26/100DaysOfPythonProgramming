fa3a02f668f253af1bac0886b0d73d60bb41e7a3
rohankshirsagar <61232180+rohankshirsagar26@users.noreply.github.com>
2023-02-26 10:52:10 +0000

    Reeborg Challenege Hurdle 4

--- Day6/main.py
+++ Day6/main.py
@@ -90,3 +90,33 @@ while not at_goal():
         skip_wall()
     elif front_is_clear():
         move()
+
+
+# Reeborg Challenege Hurdle 4
+
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def skip_wall():
+    count = 0
+    turn_left()
+    while not right_is_clear():
+        move()
+        count+=1
+    turn_right()
+    move()
+    turn_right()
+    while not count==0:
+        move()
+        count-=1
+    turn_left()
+   
+while not at_goal():
+    if wall_in_front():
+        skip_wall()
+    elif front_is_clear():
+        move()
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # While loop
 
-# Reeborg Challenege with while and if
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
 
-https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+# Reeborg Challenege Hurdle 4
 
 def turn_right():
     turn_left()
@@ -10,13 +10,18 @@ def turn_right():
     turn_left()
 
 def skip_wall():
+    count = 0
     turn_left()
-    move()
+    while not right_is_clear():
+        move()
+        count+=1
     turn_right()
     move()
     turn_right()
-    move()
-    turn_left()   
+    while not count==0:
+        move()
+        count-=1
+    turn_left()
    
 while not at_goal():
     if wall_in_front():
