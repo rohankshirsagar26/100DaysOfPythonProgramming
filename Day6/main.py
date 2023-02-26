fc7a0d6217f3f71b971c4f6f620ab2347bb93ebd
rohankshirsagar <61232180+rohankshirsagar26@users.noreply.github.com>
2023-02-26 08:51:21 +0000

    While loop

--- Day6/main.py
+++ Day6/main.py
@@ -33,3 +33,38 @@ def one_hurdle():
    
 for step in range(6):
     one_hurdle()
+
+# While loop
+
+# Reeborg Challenege
+
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def step_one():
+    move()
+    turn_left()
+    move()
+    turn_right()
+
+def step_two():
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+def one_hurdle():
+    step_one()
+    step_two()
+    
+number_of_hurdles = 6
+   
+while number_of_hurdles > 0:
+    one_hurdle()
+    if at_goal() == 'true':
+        number_of_hurdles = 0
+    number_of_hurdles -= 1
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-# Reeborg Challenge
+# While loop
 
-https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+# Reeborg Challenege
 
-# Python Code
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
 
 def turn_right():
     turn_left()
@@ -20,10 +20,15 @@ def step_two():
     turn_right()
     move()
     turn_left()
-
+    
 def one_hurdle():
     step_one()
     step_two()
-
-for step in range(6):
+    
+number_of_hurdles = 6
+   
+while number_of_hurdles > 0:
     one_hurdle()
+    if at_goal() == 'true':
+        number_of_hurdles = 0
+    number_of_hurdles -= 1
