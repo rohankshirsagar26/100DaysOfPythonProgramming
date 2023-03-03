bfde3dae7bb98010b5f17a8db37c96c53a5f7b79
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2023-03-03 10:30:30 +0530

    Better use of while loop

--- Day7/main.py
+++ Day7/main.py
@@ -71,24 +71,19 @@ print(f'Pssst, the solution is {chosen_word}.')
 #Create blanks
 display = []
 for _ in range(word_length):
-    display += "_"
+    display.append('_')
 
 #TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
 
-end_of_game = False
-
-while not end_of_game:
-
+while '_' in display:
   guess = input("Guess a letter: ").lower()
   
   #Check guessed letter
-  
   for position in range(word_length):
       letter = chosen_word[position]
       if letter == guess:
           display[position] = letter
   
   print(display)
-
-  if "_" not in display:
-    end_of_game = True
+  
+print('You win.')
