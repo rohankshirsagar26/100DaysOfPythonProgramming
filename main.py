990774c8796d0385bc13edc905eeb853ac027c59
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2023-03-04 10:30:45 +0530

    Alternate way of solving step 4 of hangman game

--- main.py
+++ main.py
@@ -59,33 +59,49 @@ stages = ['''
 =========
 ''']
 
-import random
-word_list = ["camel", "camel", "camel"]
+end_of_game = False
+word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
+
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+#Set 'lives' to equal 6.
 lives = 6
+
 #Testing code
-print(f'Pssst, the solution is {chosen_word}.\n')
+print(f'Pssst, the solution is {chosen_word}.')
 
+#Create blanks
 display = []
-
 for _ in range(word_length):
-    display.append('_')
+    display += "_"
 
-while '_' in display and lives > -1:
-  blanks = display.count('_')
-  guess = input("Guess a letter: ").lower()
-  for position in range(word_length):
-      letter = chosen_word[position]
-      if letter == guess:
-        display[position] = letter
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
-  print(display, '\n')
-  if blanks == display.count('_'):
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+       # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+    #TODO-2: - If guess is not a letter in the chosen_word,
+    #Then reduce 'lives' by 1. 
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
+
+    #Join all the elements in the list and turn it into a String.
+    print(f"{' '.join(display)}")
+
+    #Check if user has got all letters.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
     print(stages[lives])
-    lives -= 1
- 
-if lives == -1:
-  print('You lose.')
-else:
-  print('You win.')
