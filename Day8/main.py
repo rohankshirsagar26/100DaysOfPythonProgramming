125868b4aa01f00bc021e0f61effa43436b8c8d3
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2023-04-04 05:07:01 +0530

    Function with multiple inputs

--- Day8/main.py
+++ Day8/main.py
@@ -20,3 +20,13 @@ def greet(name):
         print(f"Hey {name} you are not eligible to vote")
 
 greet("John Doe")
+
+# Function with multiple inputs
+
+def canVote(name, age):
+    if age >= 18:
+        print(f"Hi {name} you are eligible to vote")
+    else:
+        print(f"Hey {name} you are NOT eligible to vote")
+
+canVote("Deepika", 32)
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-# Function with inputs
+# Function with multiple inputs
 
-def canVote(name):
-    age = int(input(f"Hey {name}, how old are you? "))
+def canVote(name, age):
     if age >= 18:
         print(f"Hi {name} you are eligible to vote")
     else:
-        print(f"Hey {name} you are not eligible to vote")
+        print(f"Hey {name} you are NOT eligible to vote")
 
-canVote("John Doe")
+canVote("Deepika", 32)
