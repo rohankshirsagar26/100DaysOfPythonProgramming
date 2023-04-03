66f8c88c042b449844aa3d0058d19a356e5a49ed
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2023-04-03 16:36:01 +0530

    Function with inputs

--- Day8/main.py
+++ Day8/main.py
@@ -9,3 +9,14 @@ def canVote():
         print(f"Hey {name} you are not eligible to vote")
 
 canVote()
+
+# Function with inputs
+
+def greet(name):
+    age = int(input(f"Hey {name}, how old are you? "))
+    if age >= 18:
+        print(f"Hi {name} you are eligible to vote")
+    else:
+        print(f"Hey {name} you are not eligible to vote")
+
+greet("John Doe")
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-# Basic Function
+# Function with inputs
 
-def canVote():
-    name = input("Enter your name: ")
-    age = int(input("How old are you? "))
+def canVote(name):
+    age = int(input(f"Hey {name}, how old are you? "))
     if age >= 18:
         print(f"Hi {name} you are eligible to vote")
     else:
         print(f"Hey {name} you are not eligible to vote")
 
-canVote()
+canVote("John Doe")
