6ff414e31c831458bf333e840e57e147e18bff5f
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2023-04-05 06:56:16 +0530

    Casesar Cipher with single caesar function

--- Day8/caesar-cipher.py
+++ Day8/caesar-cipher.py
@@ -1,34 +1,26 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-
 while not (direction == 'encode' or direction == 'decode'):
   print('Invalid input\n')
   direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(text, shift):
-  cipher_text=''
+def caesar(direction, text, shift):
+  output_text = ''
   for letter in text:
-    if (alphabet.index(letter) + shift) > 25:
-      cipher_text += alphabet[(alphabet.index(letter) + shift) - 26]
-    else:  
-      cipher_text += alphabet[(alphabet.index(letter) + shift)]
-  print(f"The encoded text is {cipher_text}")
+    if direction == 'encode':
+      if (alphabet.index(letter) + shift) > 25:
+        output_text += alphabet[(alphabet.index(letter) + shift) - 26]
+      else:  
+        output_text += alphabet[(alphabet.index(letter) + shift)]
+    if direction == 'decode':
+      if (alphabet.index(letter) + shift) > 25:
+        output_text += alphabet[(alphabet.index(letter) - shift) - 26]
+      else:  
+        output_text += alphabet[(alphabet.index(letter) - shift)]
 
+  print(f"The {direction}d text is {output_text}")
 
-def decrypt(text, shift):
-  cipher_text = ''
-  for letter in text:
-    if (alphabet.index(letter) + shift) > 25:
-      cipher_text += alphabet[(alphabet.index(letter) - shift) - 26]
-    else:  
-      cipher_text += alphabet[(alphabet.index(letter) - shift)]
-  print(f"The decoded text is {cipher_text}")
-
-if direction == 'encode':
-  encrypt(text = text, shift = shift)
-elif direction == 'decode':
-  decrypt(text = text, shift = shift)
+caesar(direction, text, shift)
