adc86b4a6961ed7d746e11c5917408089371b7e9
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2023-04-05 06:21:37 +0530

    Caesar Cipher decrypt text

--- Day8/caesar-cipher.py
+++ Day8/caesar-cipher.py
@@ -1,15 +1,34 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
+while not (direction == 'encode' or direction == 'decode'):
+  print('Invalid input\n')
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    cipher_text += alphabet[new_position]
+def encrypt(text, shift):
+  cipher_text=''
+  for letter in text:
+    if (alphabet.index(letter) + shift) > 25:
+      cipher_text += alphabet[(alphabet.index(letter) + shift) - 26]
+    else:  
+      cipher_text += alphabet[(alphabet.index(letter) + shift)]
   print(f"The encoded text is {cipher_text}")
 
-encrypt(plain_text=text, shift_amount=shift)
+
+def decrypt(text, shift):
+  cipher_text = ''
+  for letter in text:
+    if (alphabet.index(letter) + shift) > 25:
+      cipher_text += alphabet[(alphabet.index(letter) - shift) - 26]
+    else:  
+      cipher_text += alphabet[(alphabet.index(letter) - shift)]
+  print(f"The decoded text is {cipher_text}")
+
+if direction == 'encode':
+  encrypt(text = text, shift = shift)
+elif direction == 'decode':
+  decrypt(text = text, shift = shift)
