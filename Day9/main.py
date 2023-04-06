a953730277ec941b7e1f5361bbe58c47bc3cb0f0
rohankshirsagar <61232180+rohankshirsagar26@users.noreply.github.com>
2023-04-06 03:52:04 +0000

    Nesting Lists and Dictionaries

--- Day9/main.py
+++ Day9/main.py
@@ -31,3 +31,44 @@ print("empty_dictionary = ", empty_dictonary)
 # Wiping out existing dictionary
 programming_dictionary = {}
 print("programming_dictionary = ", programming_dictionary)
+
+# Nesting
+
+capitals = {
+  "France": "Paris",
+  "Germany": "Berlin"
+}
+
+# Nesting list in a dictionary
+
+travel_log = {
+  "France": ["Paris", "Lille", "Dijon"]
+}
+
+# Nesting dictionary in a dictionary
+
+travel_log = {
+  "France": {
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 12
+  },
+  "Germany": {
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgard"],
+    "total_visits": 5
+  }
+}
+
+# Nesting a dictionary in a list
+
+travel_log = [
+  {
+    "country": "France",
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 12
+  },
+  {
+    "country": "Germany",
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgard"],
+    "total_visits": 5
+  }
+]
--- main.py
+++ main.py
@@ -1,33 +1,40 @@
-# Dictionary in python
-programming_dictionary = {
-  "Bug": "An error in a program that prevents the program from running as expected.",
-  "Function": "A piece of code that you can easily call over and over again.",
-  "Loop": "The action of doing something over and over again."
+# Nesting
+
+capitals = {
+  "France": "Paris",
+  "Germany": "Berlin"
 }
 
-# Retrieving item from dictionary
-print(programming_dictionary["Function"])
+# Nesting List in a Dictionary
 
-# Adding new items to dictionary
-programming_dictionary["Dictionary"] = "A set of key value pairs"
+travel_log = {
+  "France": ["Paris", "Lille", "Dijon"]
+}
 
-# Printing dictionary
-print(programming_dictionary)
+# Nesting Dictionary in a Dictionary
 
-# Edit an item in dictionary
-print("Bug: ", programming_dictionary["Bug"])
-programming_dictionary["Bug"] = "A moth in a computer."
-print("Bug: ", programming_dictionary["Bug"])
+travel_log = {
+  "France": {
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 12
+  },
+  "Germany": {
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgard"],
+    "total_visits": 5
+  }
+}
 
-# Looping through a dictionary
-for key in programming_dictionary:
-  print(key)
-  print(programming_dictionary[key])
+# Nesting a Dictionary in a List
 
-# Creating empty dictionary
-empty_dictonary = {}
-print("empty_dictionary = ", empty_dictonary)
-
-# Wiping out existing dictionary
-programming_dictionary = {}
-print("programming_dictionary = ", programming_dictionary)
+travel_log = [
+  {
+    "country": "France",
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 12
+  },
+  {
+    "country": "Germany",
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgard"],
+    "total_visits": 5
+  }
+]
