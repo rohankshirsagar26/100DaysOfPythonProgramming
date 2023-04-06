4c1b2da2a87bf96dc4c42d8e22ab6acb949d3dc5
rohankshirsagar <61232180+rohankshirsagar26@users.noreply.github.com>
2023-04-06 01:23:08 +0000

    Adding new items to dictionary & printing dictionary

--- Day9/main.py
+++ Day9/main.py
@@ -1,9 +1,15 @@
-# Dictonary in python
+# Dictionary in python
 programming_dictionary = {
   "Bug": "An error in a program that prevents the program from running as expected.",
   "Function": "A piece of code that you can easily call over and over again.",
   "Loop": "The action of doing something over and over again."
 }
 
-# Retrieving item from dictonary
+# Retrieving item from dictionary
 print(programming_dictionary["Function"])
+
+# Adding new items to dictionary
+programming_dictionary["Dictionary"] = "A set of key value pairs"
+
+# Printing dictonary
+print(programming_dictionary)
