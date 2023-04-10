523fece06cb279de4c9f002ef1a31509db65738b
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2023-04-10 07:51:04 +0530

    Python doesn't have block scope

--- Day12/main.py
+++ Day12/main.py
@@ -25,3 +25,22 @@ def drink_potion():
     print(potion_strength)
 
 drink_potion()
+
+# There is no block scope in Pyhon
+
+game_level = 3
+enemies = ["Skeleton", "Zombie", "Alien"]
+
+if game_level < 5:
+  new_enemy = enemies[0]
+
+print(new_enemy)
+
+def create_enemy():
+  enemies = ["Skeleton", "Zombie", "Alien"]
+
+  if game_level < 5:
+    new_enemy = enemies[0]
+
+create_enemy()
+print(new_enemy)
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-# Global Scope
+# There is no block scope in Pyhon
 
-potion_strength = 2
+game_level = 3
+enemies = ["Skeleton", "Zombie", "Alien"]
 
-def drink_potion():
-    print(potion_strength)
+if game_level < 5:
+  new_enemy = enemies[0]
 
-drink_potion()
+print(new_enemy)
+
+def create_enemy():
+  enemies = ["Skeleton", "Zombie", "Alien"]
+
+  if game_level < 5:
+    new_enemy = enemies[0]
+
+create_enemy()
+print(new_enemy)
