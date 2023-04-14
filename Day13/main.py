0d5122fe9fc13a29c3b844246fcf7ec282e46cd2
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2023-04-14 11:57:15 +0530

    Use print to debug the code

--- Day13/main.py
+++ Day13/main.py
@@ -24,3 +24,11 @@ elif year > 1994:
 age = input("How old are you?")
 if age > 18:
 print("You can drive at age {age}.")
+
+#Print is Your Friend
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+word_per_page == int(input("Number of words per page: "))
+total_words = pages * word_per_page
+print(total_words)
