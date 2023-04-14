e152273ff2a98496151a8254d5a217e8f78b9581
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2023-04-14 11:50:55 +0530

    Identifying a bug

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 ############DEBUGGING#####################
 
-# Reproduce the Bug
-from random import randint
-dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-dice_num = randint(1, 6)
-print(dice_imgs[dice_num])
+# Play Computer
+year = int(input("What's your year of birth?"))
+if year > 1980 and year < 1994:
+  print("You are a millenial.")
+elif year > 1994:
+  print("You are a Gen Z.")
