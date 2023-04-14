8f86c489af30e41dd42d0d8ab40d5d5db3d8ebfc
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2023-04-14 11:56:20 +0530

    Fix the errors

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 ############DEBUGGING#####################
 
-# Play Computer
-year = int(input("What's your year of birth?"))
-if year > 1980 and year < 1994:
-  print("You are a millenial.")
-elif year > 1994:
-  print("You are a Gen Z.")
+# Fix the Errors
+age = input("How old are you?")
+if age > 18:
+print("You can drive at age {age}.")
