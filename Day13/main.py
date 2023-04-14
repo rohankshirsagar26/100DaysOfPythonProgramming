3f8d93ebcd20aae7fba937a474197d4d27649032
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2023-04-14 09:27:07 +0530

    Reproducing the bug

--- Day13/main.py
+++ Day13/main.py
@@ -6,3 +6,9 @@ def my_function():
     if i == 20:
       print("You got it")
 my_function()
+
+# Reproduce the Bug
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(1, 6)
+print(dice_imgs[dice_num])
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 ############DEBUGGING#####################
 
-# Describe Problem
-def my_function():
-  for i in range(1, 20):
-    if i == 20:
-      print("You got it")
-my_function()
+# Reproduce the Bug
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(1, 6)
+print(dice_imgs[dice_num])
