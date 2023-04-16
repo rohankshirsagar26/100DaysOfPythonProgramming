152484f6578214f870667a1e829415b292a474d3
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2023-04-16 17:00:05 +0530

    Coffee maker machine using OOP

--- Day16/main.py
+++ Day16/main.py
@@ -1,31 +1,21 @@
-# Import class from a module
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-from turtle import Turtle, Screen
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
+is_on = True
 
-# Create Object from Blueprint
-timmy = Turtle()
-print(timmy)
-
-# Access attributes of Class
-
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
-
-# Call methods associated with Object
-timmy.shape("turtle")
-timmy.color("green")
-timmy.forward(100)
-
-# 3rd party PyPi Library
-
-from prettytable import PrettyTable
-
-table = PrettyTable()
-
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Yype", ["Electric", "Water", "Fire"])
-
-table.align = "l"
-
-print(table)
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
