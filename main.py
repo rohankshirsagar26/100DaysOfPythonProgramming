e9fbdf5cc69ced44518b45f1d5929f68aeaadcce
rohankshirsagar26 <rohankshirsagar26@gmail.com>
2023-04-16 15:35:59 +0530

    Accessed canvheight attribute of Screen class

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ from turtle import Turtle, Screen
 # Create Object from Blueprint
 timmy = Turtle()
 print(timmy)
+
+# Access attributes of Class
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
